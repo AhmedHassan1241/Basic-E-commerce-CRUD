4c3a902ec3a801905a4b267ebccad0543159a52b
Ahmed Hassan <93686680+AhmedHassan1241@users.noreply.github.com>
2024-11-12 17:02:02 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         </div>
         <div className="col-10">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Products />} />
             <Route path="/products" element={<Outlet />}>
               <Route path="" element={<Products />} />
               <Route path="add" element={<AddProduct />} />
